92e369bf67458a740767528593b588d4df3a8263
Matej <matejchalachan@gmail.com>
2022-07-18 21:14:37 +0200

    added curl loading

--- src/commands/BuildController.php
+++ src/commands/BuildController.php
@@ -2,16 +2,36 @@
 
 class BuildController extends \yii\console\Controller
 {
-    public function actionInit()
+    private $baseUrl = 'https://chromium-i18n.appspot.com/ssl-address/data';
+
+    /**
+     * Initialize files examples and patterns in directory resources
+     * @return int
+     */
+    public function actionInit(): int
     {
         //@TODO
         //
-        // 1. load https://chromium-i18n.appspot.com/ssl-address/data
-
-        // 2. load patterns for countries https://chromium-i18n.appspot.com/ssl-address/data/{country code}
+        // 1. load $this->baseUrl
+        $countries = $this->fileGetContentsCurl($this->baseUrl);
+        // 2. load patterns for countries $this->baseUrl/{country code}
 
         // 3. save patterns and examples
 
         return \yii\console\ExitCode::OK;
+    }
+
+    private function fileGetContentsCurl($url)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
     }
 }
